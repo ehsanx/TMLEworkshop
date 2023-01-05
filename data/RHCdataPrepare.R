65aab2b288db1849b01682584af58d5c494a39bc
Ehsan Karim <ehsanx@users.noreply.github.com>
2023-01-05 11:51:40 -0800

    save 2 outcomes

--- data/RHCdataPrepare.R
+++ data/RHCdataPrepare.R
@@ -1,25 +1,27 @@
+setwd("~/GitHub/TMLEworkshop/")
 # load the dataset
 ObsData <- read.csv("https://hbiostat.org/data/repo/rhc.csv", header = TRUE)
 saveRDS(ObsData, file = "data/rhc.RDS")
-# add column for outcome Y: length of stay 
+# add column for outcome Y: length of stay
 # Y = date of discharge - study admission date
 # Y = date of death - study admission date if date of discharge not available
 ObsData$Y <- ObsData$dschdte - ObsData$sadmdte
-ObsData$Y[is.na(ObsData$Y)] <- ObsData$dthdte[is.na(ObsData$Y)] - 
+ObsData$Y[is.na(ObsData$Y)] <- ObsData$dthdte[is.na(ObsData$Y)] -
   ObsData$sadmdte[is.na(ObsData$Y)]
 # remove outcomes we are not examining in this example
 death <- ObsData$death
-ObsData <- dplyr::select(ObsData, 
+saveRDS(death, file = "data/rhcdeath.RDS")
+ObsData <- dplyr::select(ObsData,
                          !c(dthdte, lstctdte, dschdte, death, t3d30, dth30, surv2md1))
-# remove unnecessary and problematic variables 
-ObsData <- dplyr::select(ObsData, 
+# remove unnecessary and problematic variables
+ObsData <- dplyr::select(ObsData,
                          !c(sadmdte, ptid, X, adld3p, urin1, cat2))
 
-# convert all categorical variables to factors 
-factors <- c("cat1", "ca", "cardiohx", "chfhx", "dementhx", "psychhx", 
-             "chrpulhx", "renalhx", "liverhx", "gibledhx", "malighx", 
-             "immunhx", "transhx", "amihx", "sex", "dnr1", "ninsclas", 
-             "resp", "card", "neuro", "gastr", "renal", "meta", "hema", 
+# convert all categorical variables to factors
+factors <- c("cat1", "ca", "cardiohx", "chfhx", "dementhx", "psychhx",
+             "chrpulhx", "renalhx", "liverhx", "gibledhx", "malighx",
+             "immunhx", "transhx", "amihx", "sex", "dnr1", "ninsclas",
+             "resp", "card", "neuro", "gastr", "renal", "meta", "hema",
              "seps", "trauma", "ortho", "race", "income")
 ObsData[factors] <- lapply(ObsData[factors], as.factor)
 # convert our treatment A (RHC vs. No RHC) to a binary variable
@@ -38,19 +40,20 @@ levels(ObsData$ca) <- c("Metastatic","None","Localized (Yes)")
 ObsData$ca <- factor(ObsData$ca, levels=c("None",
                                           "Localized (Yes)","Metastatic"))
 # Rename variables
-names(ObsData) <- c("Disease.category", "Cancer", "Cardiovascular", 
-                    "Congestive.HF", "Dementia", "Psychiatric", "Pulmonary", 
-                    "Renal", "Hepatic", "GI.Bleed", "Tumor", 
-                    "Immunosupperssion", "Transfer.hx", "MI", "age", "sex", 
-                    "edu", "DASIndex", "APACHE.score", "Glasgow.Coma.Score", 
-                    "blood.pressure", "WBC", "Heart.rate", "Respiratory.rate", 
-                    "Temperature", "PaO2vs.FIO2", "Albumin", "Hematocrit", 
-                    "Bilirubin", "Creatinine", "Sodium", "Potassium", "PaCo2", 
-                    "PH", "Weight", "DNR.status", "Medical.insurance", 
-                    "Respiratory.Diag", "Cardiovascular.Diag", 
+names(ObsData) <- c("Disease.category", "Cancer", "Cardiovascular",
+                    "Congestive.HF", "Dementia", "Psychiatric", "Pulmonary",
+                    "Renal", "Hepatic", "GI.Bleed", "Tumor",
+                    "Immunosupperssion", "Transfer.hx", "MI", "age", "sex",
+                    "edu", "DASIndex", "APACHE.score", "Glasgow.Coma.Score",
+                    "blood.pressure", "WBC", "Heart.rate", "Respiratory.rate",
+                    "Temperature", "PaO2vs.FIO2", "Albumin", "Hematocrit",
+                    "Bilirubin", "Creatinine", "Sodium", "Potassium", "PaCo2",
+                    "PH", "Weight", "DNR.status", "Medical.insurance",
+                    "Respiratory.Diag", "Cardiovascular.Diag",
                     "Neurological.Diag", "Gastrointestinal.Diag", "Renal.Diag",
-                    "Metabolic.Diag", "Hematologic.Diag", "Sepsis.Diag", 
-                    "Trauma.Diag", "Orthopedic.Diag", "race", "income", 
+                    "Metabolic.Diag", "Hematologic.Diag", "Sepsis.Diag",
+                    "Trauma.Diag", "Orthopedic.Diag", "race", "income",
                     "Y", "A")
 saveRDS(ObsData, file = "data/rhcAnalytic.RDS")
-saveRDS(death, file = "data/rhcdeath.RDS")
+ObsData$death <- death
+saveRDS(ObsData, file = "data/rhc2Y.RDS")
